177d011fd7f10cc8166c845aa6c4bd41566f8e73
Monofraps <nic_ristock@live.de>
2012-12-24 23:00:05 +0100

    Initial Commit

---
+++ src/main/resources/archetype-resources/src/main/java/__artifactId__.java
@@ -0,0 +1,11 @@
+package ${package};
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ${artifactId} extends JavaPlugin {
+	public void onDisable() {
+	}
+
+	public void onEnable() {
+	}
+}
